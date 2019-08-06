2a830da768de113e263d00e7d24d367806ae2272
Piyush Priyadarshi <priyadarship85@gmail.com>
2019-08-06 19:00:24 +0530

    testing war

---
+++ src/main/java/com/syndhacathon/awsomecoders/AwsomecodersApplication.java
@@ -0,0 +1,13 @@
+package com.syndhacathon.awsomecoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsomecodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsomecodersApplication.class, args);
+	}
+
+}
